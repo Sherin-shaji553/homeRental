78e93d9c211e45d29b1cf527edc657d32c516e19
Sherin <sherinshajiktr@gmail.com>
2024-12-23 21:34:01 +0530

    initial backend setup

---
+++ server/index.js
@@ -0,0 +1,18 @@
+ import express from 'express'
+ import dotenv from 'dotenv'
+ import cors from 'cors'
+
+
+ dotenv.config()
+
+ const app = express()
+
+ app.use(express.json())
+ app.use(cors())
+ app.use(express.static("public"))
+
+ app.listen(3000, ()=>{
+    console.log("Server is running on PORT 3000");
+ })
+
+ 
